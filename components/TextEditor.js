27c30ce929aae23bea8eb69d8c1c34c191c5038e
Basil Khan <basilkhan76@gmail.com>
2021-08-01 10:43:39 -0400

    Draft Js Added. SSR for text-editor disabled

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -1,3 +1,13 @@
+import dynamic from "next/dynamic";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+const Editor = dynamic(
+  () => import("react-draft-wysiwyg").then((module) => module.Editor),
+  {
+    ssr: false,
+  }
+);
+
 function TextEditor() {
   return (
     <div>
